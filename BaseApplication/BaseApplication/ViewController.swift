2dafaa15a98d11e59cbbaf1226155a0c8c4abd5f
iosdeveloperashish <iosdeveloperashish@gmail.com>
2021-04-20 12:53:52 +0530

    Base Codebase Created

---
+++ BaseApplication/BaseApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseApplication
+//
+//  Created by Ashish Viltoriya on 20/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
